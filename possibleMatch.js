8fcf41205effe31db28bc802d504760b924b4306
Synkronizing <josh6050@gmail.com>
2023-12-31 16:05:05 -0700

    acquired sprites and created display for them

--- possibleMatch.js
+++ possibleMatch.js
@@ -1,19 +1,26 @@
 $(document).ready(function () {
     $("#enter").click(function () {
-        var matches = "";
+        var table = "";
+        var matches = [];
+        var names = [];
         var first = document.querySelector('#first').value;
         var second = document.querySelector('#second').value;
         $.each(pokemon, function (index, info) {
-            if((info[1]==first && info[2]==second) || (info[2]==first && info[1]==second)){
-                if(matches == ""){
-                    matches += "" + info[0];
-                }
-                else{
-                    matches += ",  " + info[0] + "";
-                }
-                
+            if ((info[1] == first && info[2] == second) || (info[2] == first && info[1] == second)) {
+                let name = info[0]
+                names.push(name);
+                let nameImg = name.toLowerCase() + ".png";
+                matches.push(nameImg);
             }
         });
-        $("#result").html(matches);
+        for (let i = 0; i < matches.length; i += 5) {
+            table += "<tr>";
+            for(let j = i;j<i+5 && j <matches.length;j++){
+                table += "<td class='outTable'> <img src='sprites/" + matches[j] + "' alt='" + names[j] + "'> <p class='desc'> "+ names[j] + "</p></td>";
+            }
+            table += "</tr>";
+
+        }
+        $("#result").html(table);
     });
 });
